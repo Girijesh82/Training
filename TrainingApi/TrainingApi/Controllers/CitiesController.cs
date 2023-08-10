99a08b652f373615d0f92543ee9a116564c6a50e
Girijesh82 <g.pandey82@gmail.com>
2023-08-10 06:16:35 +0530

    Add files via upload

---
+++ TrainingApi/TrainingApi/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrainingApi.Model;
+
+namespace TrainingApi.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CitiesDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<CitiesDto> GetCitiesById(int id)
+        {
+
+            var city=CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+            if (city == null)
+                return NoContent();
+            return Ok(city);
+        }
+    }
+}
---
+++ TrainingApi/TrainingApi/Model/PointOfInterestForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingApi.Model
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "Please provide a good name.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
